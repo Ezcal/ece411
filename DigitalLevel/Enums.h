f8264dbfd7e8b02bb989783de9337299dffd367c
sabmah <sabin.mhrj@gmail.com>
2015-11-01 19:40:19 -0800

    fixed the port pins

---
+++ DigitalLevel/Enums.h
@@ -0,0 +1,51 @@
+/*
+ * Enums.h
+ *
+ * Created: 10/30/2015 9:21:05 PM
+ *  Author: msabin
+ */ 
+
+
+#ifndef ENUMS_H_
+#define ENUMS_H_
+
+
+
+
+
+#endif /* ENUMS_H_ */
+
+typedef enum {
+	POS_X_AXIS,
+	NEG_X_AXIS,
+	POS_Y_AXIS,
+	NEG_Y_AXIS,
+	CENTERED,
+	NONE
+}
+axis_type;
+
+axis_type axis = NONE; // START WITH THE NONE STATE
+
+typedef enum {
+	POS_X1,
+	POS_X2,
+	POS_X3,
+	POS_X4,
+	NEG_X1,
+	NEG_X2,
+	NEG_X3,
+	NEG_X4,
+	POS_Y1,
+	POS_Y2,
+	POS_Y3,
+	POS_Y4,
+	NEG_Y1,
+	NEG_Y2,
+	NEG_Y3,
+	NEG_Y4,
+	CNTR,
+	NOTHING
+} led_axis_type;
+
+led_axis_type led = NONE; // START WITH THE NONE STATE
