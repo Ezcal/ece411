0ccf16edbb91a27414bed254dbf861b76177de4f
Sabin Maharjan <sabin.mhrj@gmail.com>
2015-11-25 19:47:11 -0800

    Update DigitalLevel.c

--- src/DigitalLevel.c
+++ src/DigitalLevel.c
@@ -12,10 +12,9 @@
 typedef enum { false = 0, true = 1  } bool;
 
 void adc_init();
-uint16_t adc_read(uint8_t);
-axis_type axis(uint16_t, plane_type);
-void led_binary(axis_type, uint16_t);
-void led_select(led_axis_type);
+unsigned int adc_read(uint16_t);
+led_axis_type find_led_axis_type(plane_type, unsigned int);
+void light_led(led_axis_type);
 
 int main(void)
 {
@@ -37,153 +36,76 @@ int main(void)
 	DDRD |= 1 << DDD7;
 	DDRB |= 1 << DDB0;
 
-	uint8_t ch_x = 2;
-	uint8_t ch_y = 1;
+	uint16_t ch_x = 0x02;
+	uint16_t ch_y = 0x01;
 
-	adc_init();
+	led_axis_type led_type = NONE; // START WITH THE NONE STATE
 
     while(1)
     {
 		// X-Axis operations
 		// Reads from ADC
 		// Truns the led on the x axis depending on the converted value
-		uint16_t digital_value_x = adc_read(ch_x);
-		axis_type axis_x = axis(digital_value_x, X_PLANE);
-		led_binary(axis_x, digital_value_x);
+		adc_init();
+		unsigned int digital_value_x = adc_read(ch_x);
+		led_type = find_led_axis_type(X_PLANE, digital_value_x);
+		light_led(led_type);
 		
 		// Y-Axis Operations
 		// Reads from ADC
 		// Turns the led on y axis depending on the converted value
-		uint16_t digital_value_y = adc_read(ch_y);
-		axis_type axis_y = axis(digital_value_y, Y_PLANE);
-		led_binary(axis_y, digital_value_y);
-		
+		adc_init();
+		unsigned int digital_value_y = adc_read(ch_y);
+		led_type = find_led_axis_type(Y_PLANE, digital_value_y);
+		light_led(led_type);
     }
-}
-
-void adc_init()
-{
-	 //AREF = AVcc
-	 ADMUX = (1<<REFS0);
-	
-	 //ADC Enable and prescaler of 128
-	 //1000000/16 = 7812.5
-	ADCSRA = (1<<ADEN)|(1<<ADPS2);
-}
-
- //Read the value of the port and spits
- //the Converted digital value
-uint16_t adc_read(uint8_t ch)
-{
-	 //select the corresponding channel 0~6
-	 //ANDing with ’6? will always keep the value
-	 //of ‘ch’ between 0 and 6
-	//ch &= 0b00000110;   AND operation with 6
-	ADMUX = (ADMUX & 0xF8)|ch; // clears the bottom 3 bits before ORing
-	
-	// start single convertion
-	 //write ’1? to ADSC
-	ADCSRA |= (1<<ADSC);
-	
-	 //wait for conversion to complete
-	 //ADSC becomes ’0? again
-	 //till then, run loop continuously
-	while(ADCSRA & (1<<ADSC));
-	
-	return (ADCW);
-}
-
-
-axis_type axis(uint16_t value, plane_type plane)
-{
-	
-	if(value < 158 && value > 149){return CENTERED;}
-		
-	if (plane == X_PLANE)
-	{
-		bool pos_x_axis = value <= 205 && value >= 158;
-		bool neg_x_axis = value <= 149 && value >= 102;
-		
-		if(pos_x_axis){
-			return POS_X_AXIS;
-		}
-		if(neg_x_axis){
-			return NEG_X_AXIS;
-		}
-	}
-	
-	if (plane == Y_PLANE)
-	{
-		bool pos_y_axis = value <= 158 && value >= 205;
-		bool neg_y_axis = value <= 149 && value >= 102;
-		
-		if(pos_y_axis){
-			return POS_Y_AXIS;
-		}
-		if(neg_y_axis){
-			return NEG_Y_AXIS;
-		}
-	}
-
-	//no range to fit the axis
-	return NONE;
-	
 }
 
 // Compare the values and Select the LED depending on the value.
 // This function calls another function that turns the LED ON.
-void led_binary(axis_type axis, uint16_t value)
+led_axis_type find_led_axis_type(plane_type plane, unsigned int value)
 {
-	PORTD &= 0 << PD0; PORTD &= 0 << PD4; PORTD &=  0 << PD5;
-	
-	switch(axis)
+
+	switch (plane)
 	{
-		case POS_X_AXIS:
-			PORTD |= 1 << PD0; 
-			if(value > 154 && value <= 158){led_select(POS_X1);}
-			if(value > 158 && value <= 163){led_select(POS_X2);}
-			if(value > 163 && value <= 173){led_select(POS_X3);}
-			if(value > 173 && value <= 205){led_select(POS_X4);}
-			break;
+		case X_PLANE:
 		
-		case NEG_X_AXIS:
-			PORTD |= 1 << PD0; 
-			if(value > 154 && value <= 149){led_select(NEG_X1);}
-			if(value > 149 && value <= 144){led_select(NEG_X2);}
-			if(value > 144 && value <= 135){led_select(NEG_X3);}
-			if(value > 135 && value <= 102){led_select(NEG_X4);}
-			break;
+			if(value > 685 && value < 710){return CENTER;}
 		
-		case POS_Y_AXIS:
-			PORTD |=  1 << PD5;
-			if(value > 154 && value <= 158){led_select(POS_Y1);}
-			if(value > 158 && value <= 163){led_select(POS_Y2);}
-			if(value > 163 && value <= 172){led_select(POS_Y3);}
-			if(value > 172 && value <= 205){led_select(POS_Y4);}
-			break;
+			if(value >= 710 && value <= 725){return (POS_X1);}
+			if(value > 725 && value <= 745){return(POS_X2);}
+			if(value > 745 && value <= 760){return(POS_X3);}
+			if(value > 760){return(POS_X4);}
+			
+			if(value > 670 && value <= 685){return(NEG_X1);}
+			if(value > 650 && value <= 670){return(NEG_X2);}
+			if(value > 625 && value <= 650){return(NEG_X3);}
+			if(value < 625){return(NEG_X4);}
+		break;
 		
-		case NEG_Y_AXIS:
-			PORTD |=  1 << PD5;
-			if(value > 154 && value <= 149){led_select(NEG_Y1);}
-			if(value > 149 && value <= 145){led_select(NEG_Y1);}
-			if(value > 145 && value <= 135){led_select(NEG_Y1);}
-			if(value > 135 && value <= 102){led_select(NEG_Y1);}
-			break;
+		case Y_PLANE:
+			
+			if(value > 680 && value < 710){return CENTER;}
+			
+			if(value >= 710 && value <= 725){return(POS_Y1);}
+			if(value > 725 && value <= 745){return(POS_Y2);}
+			if(value > 745 && value <= 760){return(POS_Y3);}
+			if(value > 760){return(POS_Y4);}
+			
+			if(value > 665 && value <= 680){return(NEG_Y1);}
+			if(value > 645 && value <= 665){return(NEG_Y1);}
+			if(value > 620 && value <= 645){return(NEG_Y1);}
+			if(value < 620){return(NEG_Y1);}
+		break;
 		
-		case CENTERED:
-			led_select(CNTR);
-			break;
-		
-		case NONE:
 		default:
-			led_select(NOTHING);
-			break;
-	}
+			return NONE;
+		break;
+	}	
 }
 
-
 // Turn ON the LED depending on the type of LED value
-void led_select(led_axis_type type)
+void light_led(led_axis_type type)
 {
 	switch(type)
 	{
@@ -251,14 +173,47 @@ void led_select(led_axis_type type)
 			PORTD |= 1 << PD6; PORTD |= 1 << PD7; PORTB |= 1 << PB0;
 			break;
 		
-		case CNTR:
+		case CENTER:
 			PORTD |= 1 << PD4;
 			break;
 		
-		case NOTHING:
+		case NONE:
 		default:
 			PORTD &= 0 << PD0; PORTD &= 0 << PD4; PORTD &=  0 << PD5;
 			break;
 	}
 
+}
+
+void adc_init()
+{
+	//AREF = AVcc
+	ADMUX = 0xc0;
+	
+	//ADC Enable and prescaler of 128
+	//1000000/16 = 7812.5
+	ADCSRA =(1<<ADPS2)|(1<<ADEN);
+}
+
+//Read the value of the port and spits
+//the Converted digital value
+unsigned int adc_read(uint16_t ch)
+{
+	//select the corresponding channel 0~6
+	//ANDing with ’6? will always keep the value
+	//of ‘ch’ between 0 and 6
+	//ch &= 0b00000110;   AND operation with 6
+	//ADMUX = (ADMUX & 0xF8)|ch; // clears the bottom 3 bits before ORing
+	ADMUX |= ch;
+	
+	// start single convertion
+	//write ’1? to ADSC
+	ADCSRA |= (1<<ADSC);
+	
+	//wait for conversion to complete
+	//ADSC becomes ’0? again
+	//till then, run loop continuously
+	while(ADCSRA & (1<<ADSC));
+	
+	return (ADCW);
 }
